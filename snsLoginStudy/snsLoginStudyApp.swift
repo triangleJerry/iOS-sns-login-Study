d33dad4561bb84afe129ccd2886db09f02050d1c
trianglejerry <trianglejerry00@gmail.com>
2025-06-13 10:44:11 +0900

    :tada: git clone 및 프로젝트 추가

---
+++ snsLoginStudy/snsLoginStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  snsLoginStudyApp.swift
+//  snsLoginStudy
+//
+//  Created by 장은석 on 6/13/25.
+//
+
+import SwiftUI
+
+@main
+struct snsLoginStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
